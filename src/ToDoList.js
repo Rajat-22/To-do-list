f0923b9dfd57220e19bffdc340afc4a7b24466e4
Rajat Sharma <rajatsharma221098@gmail.com>
2021-08-12 20:26:43 +0530

    finish

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,51 +1,46 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 function ToDoList() {
-    const [itemName,setItemName] = useState("");
-    const [lists,setLists] = useState([]);
+    const [itemName, setItemName] = useState("");
+    const [lists, setLists] = useState([]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (itemName) {
-            
-            const items =  {id:new Date().getTime().toString(),itemName};
+            const items = { id: new Date().getTime().toString(), itemName };
             console.log(items);
-           
-            setLists((items) =>{
-                return[...lists,items]
+            setLists((items) => {
+                return [...lists, items]
             });
             setLists('');
-        }else{
+        }
+        else {
             console.log('empty');
         }
-         console.log(itemName);
+        console.log(itemName);
     }
     return (
         <>
-        <article>
-            <form className='form' onSubmit={handleSubmit}>
+            <article>
+                <form className='form' onSubmit={handleSubmit}>
+                    <div className='form-control'>
+                        <label htmlFor="itemName"> ITEM :</label>
+                        <input type="text" id="itemName" name="itemName" value={itemName} onChange={(e) => setItemName(e.target.value)}></input>
+                    </div>
+                    <button type="submit">Add</button>
+                </form>
+                {
+                    lists.map((items, index) => {
+                        const { id, itemName } = items;
+                        return (
+                            <div className="item" key={id}>
+                                <h4>{itemName}</h4>
 
-            <div className='form-control'>
-                <label htmlFor="itemName"> ITEM :</label>
-                <input type="text" id="itemName" name="itemName" value={itemName} onChange={(e) => setItemName(e.target.value)}></input>
-            </div>
-
-          
-
-            <button type="submit">Add</button>
-            </form>
-            {
-                lists.map((items,index) =>{
-                    const {id,itemName} = items;
-                    return(
-                        <div className="item" key={id}>
-                            <h4>{itemName}</h4>
-                           
-                        </div>
-                    )
-                })
-            }
-        </article>
+                            </div>
+                        )
+                    })
+                }
+            </article>
 
         </>
     )
